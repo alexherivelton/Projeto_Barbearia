3004f6eeae96e3d0200e0b5f1b3af30c0ebeaeee
Christian Netto <christiannettom@gmail.com>
2025-09-27 15:56:27 -0300

    test

--- src/main/java/xela/chris/barbearia/Main.java
+++ src/main/java/xela/chris/barbearia/Main.java
@@ -1,20 +1,38 @@
 package xela.chris.barbearia;
 
+import xela.chris.barbearia.enums.PermissoesEnum;
+import xela.chris.barbearia.enums.StatusAtendimentoCliente;
+import xela.chris.barbearia.Repositorios.ClienteJson;
+import xela.chris.barbearia.Repositorios.FuncionarioJson;
 import xela.chris.barbearia.models.Cliente;
 import xela.chris.barbearia.models.Funcionario;
-import xela.chris.barbearia.negocio.Barbearia;
+
+import java.io.File;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+        ClienteJson clienteJson = new ClienteJson();
+        FuncionarioJson funcionarioJson = new FuncionarioJson();
 
-        Barbearia penteFino = new Barbearia();
+        System.out.println("Pasta dos JSONs: " + new File("jsons").getAbsolutePath());
 
-        Funcionario f1 = new Funcionario();
-        Cliente c1 = new Cliente("Rodolfo", "14027245601", "38997302679");
-        Cliente c2 = new Cliente("Rodolfo", "14027245601", "38997302679");
+        clienteJson.limpar();
+        funcionarioJson.limpar();
 
-        penteFino.adicionarCliente(c1);
-        penteFino.adicionarCliente(c2);
-        System.out.println(penteFino.getClientes());
+        Cliente cliente1 = new Cliente("Christian", "1908635986235", "193847-6543689", StatusAtendimentoCliente.EM_ESPERA);
+        Cliente cliente2 = new Cliente("xela", "1908635986235", "193847-6543689", StatusAtendimentoCliente.EM_ESPERA);
+        Funcionario funcionario1 = new Funcionario("Maria", "3498754367", "45986745", "Atendente",
+                List.of(PermissoesEnum.CADASTRAR_CLIENTE, PermissoesEnum.GERAR_NOTA));
+
+        clienteJson.adicionar(cliente1);
+        clienteJson.adicionar(cliente2);
+        funcionarioJson.adicionar(funcionario1);
+
+        System.out.println("Clientes: " + clienteJson.listar());
+        System.out.println("Funcionarios: " + funcionarioJson.listar());
+
+        System.out.println("Arquivo de clientes existe: " + new File("jsons/clientes.json").exists());
+        System.out.println("Arquivo de funcionarios existe: " + new File("jsons/funcionarios.json").exists());
     }
 }
