8707132d40469ea10fcfdac2ea5d23156b50eefb
Christian Netto <christiannettom@gmail.com>
2025-09-26 22:41:13 -0300

    feat: classe Funcionario

--- src/main/java/xela/chris/barbearia/models/Funcionario.java
+++ src/main/java/xela/chris/barbearia/models/Funcionario.java
@@ -1,7 +1,17 @@
 package xela.chris.barbearia.models;
 
+import xela.chris.barbearia.enums.StatusAtendimentoCliente;
+
 public class Funcionario extends Pessoa {
 
     private String cargo;
+    private String usuario;
+    private String senha;
 
+    public Funcionario(String nome, String cpf, String telefone,String cargo, String usuario, String senha) {
+        super(nome, cpf, telefone);
+        this.cargo = cargo;
+        this.usuario = usuario;
+        this.senha = senha;
+    }
 }
