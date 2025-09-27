d06232495ff951a83b74f723fc26cf7f449f9ed0
Christian Netto <christiannettom@gmail.com>
2025-09-26 22:42:15 -0300

    feat: classe Administrador

--- src/main/java/xela/chris/barbearia/models/Administrador.java
+++ src/main/java/xela/chris/barbearia/models/Administrador.java
@@ -1,4 +1,9 @@
 package xela.chris.barbearia.models;
 
+import xela.chris.barbearia.enums.PermissoesEnum;
+
 public class Administrador extends Funcionario {
+    public Administrador(String nome, String cpf, String telefone, String cargo, String usuario, String senha) {
+        super(nome, cpf, telefone, cargo, usuario, senha);
+    }
 }
