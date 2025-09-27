a25862f31d44ab1b8ca56d296971c8551051eba3
Christian Netto <christiannettom@gmail.com>
2025-09-26 22:41:54 -0300

    feat: classe Agenda

--- src/main/java/xela/chris/barbearia/negocio/Agenda.java
+++ src/main/java/xela/chris/barbearia/negocio/Agenda.java
@@ -1,4 +1,13 @@
 package xela.chris.barbearia.negocio;
 
+import java.util.List;
+
 public class Agenda {
+    List<Agendamento> agendamentos;
+
+
+    public void adicionarAgendamento(Agendamento agendamento) {}
+    public void removerAgendamento(Agendamento agendamento) {}
+    public List<Agendamento> verAgenda() {return agendamentos;}
+    public List<Agendamento> verAgendamentoLivres(){return agendamentos;}
 }
