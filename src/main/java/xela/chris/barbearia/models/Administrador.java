0bde9972a786127fac3e9f7010ca7dd2fbc39b9a
Christian Netto <christiannettom@gmail.com>
2025-10-08 17:44:33 -0300

    feat: adiciona sistema de permissoes na classe Admnistrador

--- src/main/java/xela/chris/barbearia/models/Administrador.java
+++ src/main/java/xela/chris/barbearia/models/Administrador.java
@@ -2,12 +2,42 @@ package xela.chris.barbearia.models;
 
 import xela.chris.barbearia.enums.PermissoesEnum;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
- * Representa um administrador da barbearia, estendendo a classe {@link Pessoa}.
+ * Representa um administrador do sistema.
  * <p>
- * Cada admnistrador possui um identificador único gerado automaticamente,
- * além de informações como cargo, usuário, senha e permissões de acesso.
+ * A classe {@code Administrator} herda de {@link Funcionario} e possui todas as permissões disponíveis no sistema.
+ * </p>
+ * <p>
+ * Um administrador pode executar qualquer ação sem restrições.
  * </p>
  */
 public class Administrador extends Funcionario {
+
+    /**
+     * Retorna todas as permissões disponíveis no sistema.
+     *
+     * @return uma lista contendo todos os valores do enum {@link PermissoesEnum}
+     */
+    @Override
+    public List<PermissoesEnum> getPermissoes() {
+        return Arrays.asList(PermissoesEnum.values());
+    }
+
+
+    /**
+     * Verifica se o administrador possui uma determinada permissão.
+     * <p>
+     * Para a classe {@code Administrator}, este método sempre retorna {@code true},
+     * pois administradores possuem todas as permissões.
+     * </p>
+     *
+     * @param permissao a permissão a ser verificada
+     * @return {@code true} sempre, já que administradores possuem todas as permissões
+     */
+    public boolean temPermissao(PermissoesEnum permissao) {
+        return true;
+    }
 }
