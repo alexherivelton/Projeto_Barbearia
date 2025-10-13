aa6b699917ba9bf21a275105bd7bf618b4a25154
alexherivelton <alexherivelton@gmail.com>
2025-10-13 10:56:48 -0300

    Salva alterações locais antes do pull

--- src/main/java/xela/chris/barbearia/Main.java
+++ src/main/java/xela/chris/barbearia/Main.java
@@ -1,5 +1,6 @@
 package xela.chris.barbearia;
 
+import xela.chris.barbearia.Gerenciadores.GerenciarVenda;
 import xela.chris.barbearia.enums.PermissoesEnum;
 import xela.chris.barbearia.enums.StatusAtendimentoCliente;
 import xela.chris.barbearia.Gerenciadores.GerenciarCliente;
@@ -8,6 +9,7 @@ import xela.chris.barbearia.Gerenciadores.GerenciadorProduto;
 import xela.chris.barbearia.models.Cliente;
 import xela.chris.barbearia.models.Funcionario;
 import xela.chris.barbearia.models.Produto;
+import xela.chris.barbearia.models.Venda;
 
 import java.io.File;
 import java.util.List;
@@ -19,12 +21,15 @@ public class Main {
         GerenciarCliente gerenciarCliente = new GerenciarCliente();
         GerenciadorFuncionario gerenciadorFuncionario = new GerenciadorFuncionario();
         GerenciadorProduto gerenciadorProduto = new GerenciadorProduto();
+        GerenciarVenda gerVenda = new GerenciarVenda();
+        gerVenda.carregar();
 
         System.out.println("Pasta dos JSONs: " + new File("jsons").getAbsolutePath());
 
         gerenciarCliente.limpar();
         gerenciadorFuncionario.limpar();
         gerenciadorProduto.limpar();
+        gerVenda.limpar();
 
         Cliente cliente1 = new Cliente("Christian", "17600724600", "33998642761", StatusAtendimentoCliente.EM_ESPERA);
         Cliente cliente2 = new Cliente("xela", "14027245601", "122234589090", StatusAtendimentoCliente.EM_ESPERA);
@@ -33,7 +38,7 @@ public class Main {
         Funcionario f2 = new Funcionario("Alberto", "87654571232", "45986745", "Atendente",
                 List.of(PermissoesEnum.CADASTRAR_CLIENTE, PermissoesEnum.GERAR_NOTA));
 
-        Produto p1 = new Produto("Shampoo", 14.5, 10);
+        Produto p1 = new Produto("Shampoo", 14.5, 5);
 
         gerenciarCliente.adicionar(cliente1);
         gerenciarCliente.adicionar(cliente2);
@@ -41,18 +46,21 @@ public class Main {
         gerenciadorFuncionario.adicionar(f2);
         gerenciadorProduto.adicionar(p1);
 
-        System.out.println("\nClientes: "  + gerenciarCliente.listar());
+        gerVenda.adicionar(new Venda(p1, 2, "10102025"));
+        System.out.println("Total arrecadado: " + gerVenda.listar());
+        System.out.println(gerenciadorProduto.listar());
 
-        System.out.println("=================================================================");
+//        System.out.println("\nClientes: "  + gerenciarCliente.listar());
+//
+//        System.out.println("=================================================================");
+//
+//        System.out.println("Funcionarios: " + gerenciadorFuncionario.listar());
+//
+//        System.out.println("Produtos: " + gerenciadorProduto.listar());
+//
+//        System.out.println("=================================================================");
 
-        System.out.println("Funcionarios: " + gerenciadorFuncionario.listar());
 
-        System.out.println("Produtos: " + gerenciadorProduto.listar());
-
-        System.out.println("=================================================================");
-
-//        clienteJson.removerPorCpf("14027245601");
-//        System.out.println(clienteJson.listar());
 
 
     }
