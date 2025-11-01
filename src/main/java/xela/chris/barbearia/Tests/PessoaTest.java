94e2c9258f9535ccacf2f7ca66effcbfbf5621fd
Christian Netto <christiannettom@gmail.com>
2025-11-01 16:14:07 -0300

    feat: adiciona classe para teste de funcionalidades de venda/produto e cliente e funcionarios

---
+++ src/main/java/xela/chris/barbearia/Tests/PessoaTest.java
@@ -0,0 +1,52 @@
+package xela.chris.barbearia.Tests;
+
+import xela.chris.barbearia.Gerenciadores.GerenciadorFuncionario;
+import xela.chris.barbearia.Gerenciadores.GerenciarCliente;
+import xela.chris.barbearia.enums.PermissoesEnum;
+import xela.chris.barbearia.enums.StatusAtendimentoCliente;
+import xela.chris.barbearia.models.Cliente;
+import xela.chris.barbearia.models.Funcionario;
+
+import java.io.File;
+import java.util.List;
+
+public class PessoaTest {
+    public static void main(String[] args) {
+        GerenciarCliente gerenciarCliente = new GerenciarCliente();
+        GerenciadorFuncionario gerenciadorFuncionario = new GerenciadorFuncionario();
+
+
+        System.out.println("Pasta dos JSONs: " + new File("jsons").getAbsolutePath());
+        gerenciarCliente.limpar();
+        gerenciadorFuncionario.limpar();
+
+        Cliente cliente1 = new Cliente("Christian", "17600724600", "33998642761", StatusAtendimentoCliente.EM_ESPERA);
+        Cliente cliente2 = new Cliente("xela", "14027245601", "99999999999", StatusAtendimentoCliente.EM_ESPERA);
+        Funcionario f1 = new Funcionario("Maria", "12345678900", "33998642761", "Atendente","chris" , "1234");
+        Funcionario f2 = new Funcionario("Alberto", "87654571232", "45986745", "Atendente","xela" , "mamabola");
+        Funcionario f3 = new Funcionario("Caralho", "87654571232", "11111111111", "Administrador","carambolas" , "carambolas123");
+        Funcionario f4 = new Funcionario("Jose du Corte", "12312312322", "33998642761", "Barbeiro", "mamaBolas", "tomanocuminhamaevai");
+
+        gerenciarCliente.adicionar(cliente1);
+        gerenciarCliente.adicionar(cliente2);
+
+        gerenciadorFuncionario.adicionarFuncionario(f1);
+        gerenciadorFuncionario.adicionarFuncionario(f2);
+        gerenciadorFuncionario.adicionarFuncionario(f3);
+        gerenciadorFuncionario.adicionarFuncionario(f4);
+
+
+        System.out.println("\nClientes: "  + gerenciarCliente.listar());
+
+        System.out.println("=================================================================");
+        System.out.println("=================================================================");
+
+        System.out.println("Funcionarios: " + gerenciadorFuncionario.listar());
+
+        //gerenciarCliente.removerPorCpf("17600724600");
+
+        System.out.println("\nClientes: "  + gerenciarCliente.listar());
+
+
+    }
+}
---
+++ src/main/java/xela/chris/barbearia/Tests/TestProdutoVenda.java
@@ -0,0 +1,41 @@
+package xela.chris.barbearia.Tests;
+
+import xela.chris.barbearia.Gerenciadores.GerenciadorProduto;
+import xela.chris.barbearia.Gerenciadores.GerenciarVenda;
+import xela.chris.barbearia.models.Produto;
+import xela.chris.barbearia.servicos.ServicoVenda;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class TestProdutoVenda {
+    public static void main(String[] args) {
+        GerenciadorProduto gp = new GerenciadorProduto();
+        GerenciarVenda gv = new GerenciarVenda();
+        ServicoVenda sv = new ServicoVenda(gp, gv);
+
+        //Formatar Data
+        LocalDate data = LocalDate.now();
+        DateTimeFormatter formatarData = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        String dataFormatada = data.format(formatarData);
+
+        Produto p1 = new Produto("Shampoo", 14.5, 35);
+        Produto p2 = new Produto("Gel de cabelo", 12.5, 35);
+
+        gp.adicionar(p1);
+        gp.adicionar(p2);
+
+        System.out.println("Produtos: " + gp.listar());
+
+        System.out.println("=================================================================");
+
+        sv.efetuarVenda(1, 2, dataFormatada);
+        sv.efetuarVenda(2, 2, dataFormatada);
+
+
+        System.out.println("=================================================================");
+        System.out.println(gv.buscarVendaPorId(1));
+        System.out.println(gv.buscarVendaPorId(2));
+
+    }
+}
